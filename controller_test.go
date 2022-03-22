80559fb4cddd2a83820d27d7463be2c7cfb7cdd2
Soma Rádóczi <mauserzjeh@gmail.com>
2022-03-22 23:18:13 +0100

    tests

--- controller_test.go
+++ controller_test.go
@@ -22,35 +22,116 @@
 
 package controller
 
-import "testing"
+import (
+	"errors"
+	"testing"
+	"time"
+)
+
+// Helpers ----------------------------------------------------------
+
+// assertEqual fails if the two values are not equal
+func assertEqual[T comparable](t *testing.T, got, want T) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got: %v != want: %v", got, want)
+	}
+}
+
+// assertNotEqual fails if the two values are equal
+func assertNotEqual[T comparable](t *testing.T, got, want T) {
+	t.Helper()
+	if got == want {
+		t.Errorf("didn't want %v", got)
+	}
+}
+
+// taskCallback simulates a callback. Duration parameter sets how long the function
+// takes to process (ms). Fail parameter sets if the result should end up with an error
+func taskCallback(i, duration int64, fail bool) Result {
+	if duration > 0 {
+		time.Sleep(time.Duration(duration) * time.Millisecond)
+	}
+
+	if fail {
+		return Result{
+			Output: nil,
+			Err:    errors.New("taskCallback failed"),
+		}
+	}
+
+	return Result{
+		Output: i * 2,
+		Err:    nil,
+	}
+}
+
+// Tests ------------------------------------------------------------
 
 func TestNewController(t *testing.T) {
-	c1 := New(0)
-	if c1 == nil {
-		t.Errorf("Controller is not initialized: %v", c1)
-	}
-
-	c2 := New(1)
-	if c2 == nil {
-		t.Errorf("Controller is not initialized: %v", c2)
-	}
-
+	c := New(0)
+	assertNotEqual(t, c, nil)
 }
 
 func TestGetWorkers(t *testing.T) {
 	want := uint(0)
-	c1 := New(want)
-	got := c1.GetWorkers()
+	c := New(want)
+	got := c.GetWorkers()
 
-	if int(want) != got {
-		t.Errorf("Wrong amount of workers: %v != %v", want, got)
-	}
+	assertEqual(t, got, int(want))
+}
 
-	want = 10
-	c2 := New(want)
-	got = c2.GetWorkers()
+func TestSetWorkers(t *testing.T) {
+	want := uint(0)
+	c := New(want)
+	got := c.GetWorkers()
+	assertEqual(t, got, int(want))
 
-	if int(want) != got {
-		t.Errorf("Wrong amount of workers: %v != %v", want, got)
-	}
+	want = uint(10)
+	c.SetWorkers(want)
+	got = c.GetWorkers()
+	assertEqual(t, got, int(want))
+
+	c.Start()
+
+	want = uint(5)
+	c.SetWorkers(want)
+	got = c.GetWorkers()
+	assertEqual(t, got, int(want))
+
+	want = uint(10)
+	c.SetWorkers(want)
+	got = c.GetWorkers()
+	assertEqual(t, got, int(want))
+
+	c.Stop()
+}
+
+func TestIsRunning(t *testing.T) {
+	c := New(10)
+	assertEqual(t, c.IsRunning(), false)
+
+	c.Start()
+	assertEqual(t, c.IsRunning(), true)
+
+	c.Stop()
+	assertEqual(t, c.IsRunning(), false)
+}
+
+func TestDoubleStart(t *testing.T) {
+	w := 10
+	c := New(uint(w))
+	assertEqual(t, c.IsRunning(), false)
+	assertEqual(t, c.GetWorkers(), w)
+
+	c.Start()
+	assertEqual(t, c.IsRunning(), true)
+	assertEqual(t, c.GetWorkers(), w)
+
+	c.Start()
+	assertEqual(t, c.IsRunning(), true)
+	assertEqual(t, c.GetWorkers(), w)
+
+	c.Stop()
+	assertEqual(t, c.IsRunning(), false)
 }
